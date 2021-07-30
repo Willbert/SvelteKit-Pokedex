0db4d08337024367d4cd3a09fcc545b386628731
Wilbert <wilbert_evans@hotmail.com>
2021-07-30 16:37:23 +0100

    Finish sveltkit app with routes for individual pokemon + styling with tailwind

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,7 +1,12 @@
 <script>
   export let pokeman;
+  import { fade } from 'svelte/transition'
 </script>
-<a href={`/pokemon/${pokeman.id}`}>
-  <img src="{pokeman.image}" alt={pokeman.name} />
+
+<a
+  transition:fade
+  class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+  href={`/pokemon/${pokeman.id}`}>
+  <img class="h-30" src="{pokeman.image}" alt={pokeman.name} />
   <h2>{pokeman.id}. {pokeman.name}</h2>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,53 @@
-<script>
-  import { pokemon } from '../stores/pokestore';
-  import PokemanCard from '../components/pokemanCard.svelte'
+<script context="module">
+  export async function load({page}) {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=251';
+    const res = await fetch(url);
+    const data = await res.json();
+    const loadedPokemon = data.results.map((data, index) => {
+      return {
+        name: data.name,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+      };
+    });
 
-  console.log($pokemon);
+    return {props: {pokemon:loadedPokemon}}
+  }
 </script>
+
+<script>
+  import PokemanCard from '../components/pokemanCard.svelte'
+  export let pokemon
+
+  let searchTerm = ''
+  let filteredPokemon = []
+
+  $:  {
+    if(searchTerm) {
+      filteredPokemon = pokemon.filter(
+        pokeman => pokeman.name.includes(searchTerm)
+      )
+    } else {
+      filteredPokemon = [...pokemon]
+    }
+  }
+</script>
+
 <svelte:head>
   <title>Pokedex Svelte kit</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
-{#each $pokemon as pokeman}
-  <PokemanCard pokeman={pokeman}/>
-{/each}
+<input
+  bind:value={searchTerm}
+  class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+  type="text"
+  placeholder="Search"
+/>
+
+<div class="py-4 grid gap-4 md:grid-cols-3 grid-cols-1" >
+  {#each filteredPokemon as pokeman}
+    <PokemanCard pokeman={pokeman} />
+  {/each}
+</div>
